2f49c01c20648178b279800ce42c9759b3c9297f
domivarga <v.domi26@gmail.com>
2020-05-18 13:02:23 +0200

    Create history list

--- app/src/main/java/com/example/a4cast/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/a4cast/ui/home/HomeActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a4cast.R
 import com.example.a4cast.injector
+import com.example.a4cast.model.City
 import com.example.a4cast.model.WeatherDTO
 import com.example.a4cast.ui.detail.DetailActivity
 import com.example.a4cast.ui.home.NavigationKeys.KEY_HUMIDITY
@@ -21,9 +24,11 @@ class HomeActivity : AppCompatActivity(), HomeScreen {
 
     @Inject
     lateinit var homePresenter: HomePresenter
-    lateinit var searchBar: EditText
 
-    lateinit var weatherDTO: WeatherDTO
+    private lateinit var searchBar: EditText
+    private lateinit var listView: ListView
+    private lateinit var arrayAdapter: ArrayAdapter<String?>
+    private lateinit var historyItems: List<City>
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -34,16 +39,15 @@ class HomeActivity : AppCompatActivity(), HomeScreen {
         homePresenter.attachScreen(this)
 
         setupSearchBar()
+        setupListView()
     }
 
     override fun onStart() {
         super.onStart()
-
-        homePresenter.loadWeatherData()
     }
 
     override fun displayWeatherData(weatherDTO: WeatherDTO) {
-        this.weatherDTO = weatherDTO
+        navigateToDetails(weatherDTO)
     }
 
     override fun showDetailedWeather(weatherDTO: WeatherDTO) {
@@ -52,9 +56,11 @@ class HomeActivity : AppCompatActivity(), HomeScreen {
         intent.putExtra(NavigationKeys.KEY_TEMPERATURE, weatherDTO.temp)
         intent.putExtra(NavigationKeys.KEY_HUMIDITY, weatherDTO.hum)
         startActivity(intent)
+
+        homePresenter?.loadHistory()
     }
 
-    private fun navigateToDetails() {
+    private fun navigateToDetails(weatherDTO: WeatherDTO) {
         homePresenter?.showDetail(weatherDTO)
     }
 
@@ -63,12 +69,34 @@ class HomeActivity : AppCompatActivity(), HomeScreen {
 
         searchBar.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                navigateToDetails()
-                homePresenter.loadWeatherData()
+                homePresenter.loadWeatherData(searchBar.text.toString())
                 return@setOnEditorActionListener true
             }
             return@setOnEditorActionListener false
         }
+    }
+
+    override fun showHistoryList(cityList: List<City>) {
+        historyItems = cityList
+        val listItems = arrayOfNulls<String>(cityList.size)
+
+        for (i in 0 until cityList.size) {
+            listItems[i] = cityList[i].name
+        }
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = arrayAdapter
+
+        val context = this
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedCity = historyItems[position]
+            navigateToDetails(WeatherDTO(selectedCity.temperature, selectedCity.humidity, selectedCity.name))
+        }
+    }
+
+    private fun setupListView() {
+        listView = findViewById<ListView>(R.id.history_list)
+        homePresenter?.loadHistory()
     }
 }
 
